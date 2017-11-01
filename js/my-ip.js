2bc37823c301dc575c9f39fadb50cde1144ec923
0z0ne <lgt_livezone@live.com>
2017-11-01 22:05:52 +0800

    add ip check script

--- js/my-ip.js
+++ js/my-ip.js
@@ -4,6 +4,13 @@ const cheerio = require('cheerio')
 const API = 'http://2017.ip138.com/ic.asp'
 
 request(API, function (error, response, body) {
+    if (error || !body) {
+        console.log("测试接口不可用")
+        return
+    }
     let $ = cheerio.load(body)
-    console.log($('center').text())
+    let matcher = $('center').text().match(/.*\[(.*)\].*/)
+    if (matcher) {
+        console.log('WAN IP: %s', matcher[1])
+    }
 });
