2318c45e15bb2bbbdc3ff43699c2949451909cc3
Guotao.Liu <lgt_livezone@live.com>
2017-11-02 14:34:05 +0800

    add android project

---
+++ android/Back/app/src/main/java/crazyman/top/back/MainActivity.java
@@ -0,0 +1,23 @@
+package crazyman.top.back;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent home = new Intent(Intent.ACTION_MAIN);
+        home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        home.addCategory(Intent.CATEGORY_HOME);
+        startActivity(home);
+    }
+}
