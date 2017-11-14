0649f737c88d7bc28117544b1b1cd4b93899364f
Guotao.Liu <lgt_livezone@live.com>
2017-11-14 10:40:06 +0800

    add idea license script

--- sh/daemon.sh
+++ sh/daemon.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PROG_PATH=""
-PROG_NAME=""
+PROG_PATH="/opt/idea"
+PROG_NAME="idea-license-server"
 PROD_LOG="run.log"
 RUN=0
-TICK=60
+TICK=600
 
 function run-prog () {
     $PROG_PATH/$PROG_NAME -p 12345 -u ZZJR 2>/dev/null 1>&2 &
@@ -16,10 +16,10 @@ while true; do
         RUN=0
     fi
     RUN=1
+    echo "`date '+%Y-%m-%d %H:%M:%S'` -- check status" >> $PROG_PATH/$PROD_LOG
     if [ `ps aux | grep $PROG_NAME | grep -v grep | wc -l` -lt 1 ]; then
         run-prog
-        echo `date +%Y%m%d %H%M%S` >> $PROG_PATH/$PROD_LOG
-        echo "start $PROG_NAME success." >> $PROG_PATH/$PROD_LOG
+        echo "`date '+%Y-%m-%d %H:%M:%S'` -- start $PROG_NAME success." >> $PROG_PATH/$PROD_LOG
     fi
 done
 exit 0
---
+++ sh/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nohup /opt/idea/daemon.sh >> /opt/idea/nohup.log 2>&1 &
+exit 0
