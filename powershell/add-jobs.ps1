494cc940d84964fbe8b68f57a1ab9ebd3933fc3f
huangyou <huangyou@2dfire.com>
2019-01-23 15:54:38 +0800

    update

--- powershell/add-jobs.ps1
+++ powershell/add-jobs.ps1
@@ -1,6 +1,6 @@
-$JOB_NAME = "Update Networx"
-$JOB_FILE = "D:\Bin\task-update-networx.ps1"
+$JOB_NAME = "PS_HOURLY"
+$JOB_FILE = "D:\Bin\tasks\hourly.ps1"
 
 Register-ScheduledJob -Name $JOB_NAME -FilePath $JOB_FILE
-$TIGGER = New-JobTrigger -Once -At "20:00" -RepetitionInterval "00:01:00" -RepeatIndefinitely
+$TIGGER = New-JobTrigger -Once -At "19:00" -RepetitionInterval "01:00:00" -RepeatIndefinitely
 Add-JobTrigger -Name $JOB_NAME -Trigger $TIGGER
